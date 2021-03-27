cb997c93f88a8b17f8fc025068e00a36b5e97ba3
NenadR <NRadomilja@dspace.hr>
2021-03-27 10:53:20 +0100

    alo

--- main.c
+++ main.c
@@ -7,9 +7,14 @@ int main()
 printf("Hello World\n");
 printf("Added locally\n");
 printf("Added 2 locally\n");
+printf("Added 2a locally\n");
 printf("Added 3 locally\n");
 printf("Goodbye World\n");
 }
 
 /* Footer comment */
 /* Footer comment2 */
+
+
+/* Footer comment3 */
+/* Footer comment4 */
