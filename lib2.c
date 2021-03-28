4e0b2fc24349481fa0b7d7d90955cf00e635e31f
Nenad Radomilja <neno.stobrec@gmail.com>
2021-03-28 15:52:10 +0200

    dsa

---
+++ lib2.c
@@ -0,0 +1,26 @@
+
+double Square(double x)
+{
+	return (x*x);
+}
+
+double Cube(double x)
+{
+	return (x*x*x);
+}
+
+double Divide(double x, double y)
+{
+	return (x/y);
+}
+
+double Add(double x, double y)
+{
+	return (x+y);
+}
+
+
+double Add2(double x, double y)
+{
+	return (x+y);
+}
