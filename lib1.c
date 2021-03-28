8ffb0780338c6a09b01e3776c0c2d619f4a6f94d
Nenad Radomilja <neno.stobrec@gmail.com>
2021-03-28 15:06:30 +0200

    3

--- lib1.c
+++ lib1.c
@@ -1,19 +1,22 @@
 
 double Square(double x)
 {
-	return x*x;
+	return (x*x);
 }
 
 double Cube(double x)
 {
-	return x*x*x;
+	return (x*x*x);
 }
 
 double Divide(double x, double y)
 {
-	return x/y;
+	return (x/y);
 }
-double Subtact(double x, double y)
+
+double Subtract(double x, double y)
 {
-	return x-y;
+	return (x-y);
 }
+
+//unix formating
